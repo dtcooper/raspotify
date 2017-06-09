1d1d95ff35755860f81076e1cc7376935101bf67
David Cooper <david@dtcooper.com>
2017-06-09 01:01:03 -0400

    Make install script work on really minimal raspbian (ie docker)

--- install.sh
+++ install.sh
@@ -2,10 +2,31 @@
 
 # Install script for Raspotify. Downloads and installs Debian package.
 
-if ! which lsb_release >/dev/null || lsb_release -si | fgrep -ivq raspbian; then
+DID_APT_GET_UPDATE=
+
+do_apt_get_update() {
+    if [ -z "$DID_APT_GET_UPDATE" ]; then
+        sudo apt-get update
+        DID_APT_GET_UPDATE=1
+    fi
+}
+
+if ! which lsb_release > /dev/null; then
+    do_apt_get_update
+    sudo apt-get install -y lsb-release
+fi
+
+if lsb_release -si | fgrep -ivq raspbian; then
     echo "Please run on a Raspberry Pi"
     exit 1
 fi
+
+
+if ! which curl > /dev/null; then
+    do_apt_get_update
+    sudo apt-get install -y curl
+fi
+
 
 LATEST_RELEASE="$(curl -Ls https://api.github.com/repos/dtcooper/raspotify/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4)"
 if [ -z "$LATEST_RELEASE" ]; then
@@ -13,6 +34,7 @@ if [ -z "$LATEST_RELEASE" ]; then
     exit 1
 fi
 
+do_apt_get_update
 sudo apt-get install -y libasound2 adduser
 
 cd /tmp
