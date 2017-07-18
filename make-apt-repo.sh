f15fc8fb328e62650f0e43788b92ba840062f327
David Cooper <david@dtcooper.com>
2017-07-17 23:28:51 -0700

    Add in raspotify-latest.deb symlink

--- make-apt-repo.sh
+++ make-apt-repo.sh
@@ -34,6 +34,8 @@ EOF
 reprepro includedeb jessie "../$DEB_PKG_NAME"
 rm -rf conf db
 
+ln -s "$(find . -name '*.deb' -type f -printf '%P\n' -quit)" raspotify-latest.deb
+
 # Perm fixup. Not needed on macOS, but is on Linux
 chown -R "$PERMFIX_UID:$PERMFIX_GID" /mnt/raspotify/* 2> /dev/null
 
