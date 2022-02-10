4a5b7a974ca6881e8379064791605eff87465da4
Automated Release <automated-release@users.noreply.github.com>
2022-02-10 23:45:32 +0000

    Update gh-pages apt repository (released tag 0.31.10)

--- install.sh
+++ install.sh
@@ -11,9 +11,13 @@ HELPER_MIN_VER="1.6"
 LIBASOUND_MIN_VER="1.2.4"
 LIBPULSE_MIN_VER="14.2"
 
-MAYBE_SUDO="sudo"
+SUDO="sudo"
+APT="apt"
 
 REQ_PACKAGES="systemd init-system-helpers libasound2 libpulse0 curl apt-transport-https"
+
+PACKAGES_TO_INSTALL=
+MIN_NOT_MET=
 
 if ! which apt-get apt-key > /dev/null; then
     echo "Unspported OS:"
@@ -28,7 +32,7 @@ if uname -a | grep -F -ivq -e armv7 -e aarch64 -e x86_64; then
 fi
 
 if ! which sudo > /dev/null; then
-    MAYBE_SUDO=""
+    SUDO=""
     if [ "$(id -u)" -ne 0 ]; then
         echo "Insufficient privileges:"
         echo "Please run this script as root."
@@ -36,47 +40,55 @@ if ! which sudo > /dev/null; then
     fi
 fi
 
-$MAYBE_SUDO apt-get update
-$MAYBE_SUDO apt-get -y install $REQ_PACKAGES
+if ! which apt > /dev/null; then
+    APT="apt-get"
+fi
 
-SYSTEMD_VER="$(dpkg-query -W -f='${Version}' systemd)"
-if eval dpkg --compare-versions "$SYSTEMD_VER" lt "$SYSTEMD_MIN_VER"; then
-    echo "$MIN_NOT_MET_MESG"
-    echo "systemd (>= $SYSTEMD_MIN_VER) but $SYSTEMD_VER is installed."
-    echo "$ERROR_MESG"
-    exit 1
+for package in $REQ_PACKAGES; do
+    if ! dpkg-query -W -f='${db:Status-Status}\n' "$package" 2> /dev/null | grep -q '^installed$'; then
+        PACKAGES_TO_INSTALL="$PACKAGES_TO_INSTALL $package"
+    fi
+done
+
+if [ "$PACKAGES_TO_INSTALL" ]; then
+    $SUDO $APT update
+    $SUDO $APT -y install $PACKAGES_TO_INSTALL
+
 fi
 
 HELPER_VER="$(dpkg-query -W -f='${Version}' init-system-helpers)"
 if eval dpkg --compare-versions "$HELPER_VER" lt "$HELPER_MIN_VER"; then
-    echo "$MIN_NOT_MET_MESG"
-    echo "init-system-helpers (>= $HELPER_MIN_VER) but $HELPER_VER is installed."
-    echo "$ERROR_MESG"
-    exit 1
-fi
-
-LIBASOUND_VER="$(dpkg-query -W -f='${Version}' libasound2)"
-if eval dpkg --compare-versions "$LIBASOUND_VER" lt "$LIBASOUND_MIN_VER"; then
-    echo "$MIN_NOT_MET_MESG"
-    echo "libasound2 (>= $LIBASOUND_MIN_VER) but $LIBASOUND_VER is installed."
-    echo "$ERROR_MESG"
-    exit 1
+    MIN_NOT_MET="init-system-helpers >= $HELPER_MIN_VER is required but $HELPER_VER is installed."
 fi
 
 LIBPULSE_VER="$(dpkg-query -W -f='${Version}' libpulse0)"
 if eval dpkg --compare-versions "$LIBPULSE_VER" lt "$LIBPULSE_MIN_VER"; then
+    MIN_NOT_MET="libpulse0 >= $LIBPULSE_MIN_VER is required but $LIBPULSE_VER is installed.\n$MIN_NOT_MET"
+fi
+
+LIBASOUND_VER="$(dpkg-query -W -f='${Version}' libasound2)"
+if eval dpkg --compare-versions "$LIBASOUND_VER" lt "$LIBASOUND_MIN_VER"; then
+    MIN_NOT_MET="libasound2 >= $LIBASOUND_MIN_VER is required but $LIBASOUND_VER is installed.\n$MIN_NOT_MET"
+fi
+
+SYSTEMD_VER="$(dpkg-query -W -f='${Version}' systemd)"
+if eval dpkg --compare-versions "$SYSTEMD_VER" lt "$SYSTEMD_MIN_VER"; then
+    MIN_NOT_MET="systemd >= $SYSTEMD_MIN_VER is required but $SYSTEMD_VER is installed.\n$MIN_NOT_MET"
+fi
+
+if [ "$MIN_NOT_MET" ]; then
     echo "$MIN_NOT_MET_MESG"
-    echo "libpulse0 (>= $LIBPULSE_MIN_VER) but $LIBPULSE_VER is installed."
+    echo "$MIN_NOT_MET"
     echo "$ERROR_MESG"
     exit 1
 fi
 
-curl -sSL https://dtcooper.github.io/raspotify/key.asc | $MAYBE_SUDO tee /usr/share/keyrings/raspotify_key.asc > /dev/null
-$MAYBE_SUDO chmod 644 /usr/share/keyrings/raspotify_key.asc
-echo "$SOURCE_REPO" | $MAYBE_SUDO tee /etc/apt/sources.list.d/raspotify.list
+curl -sSL https://dtcooper.github.io/raspotify/key.asc | $SUDO tee /usr/share/keyrings/raspotify_key.asc > /dev/null
+$SUDO chmod 644 /usr/share/keyrings/raspotify_key.asc
+echo "$SOURCE_REPO" | $SUDO tee /etc/apt/sources.list.d/raspotify.list
 
-$MAYBE_SUDO apt-get update
-$MAYBE_SUDO apt-get -y install raspotify
+$SUDO $APT update
+$SUDO $APT -y install raspotify
 
 echo "Thanks for install Raspotify! Don't forget to checkout the wiki for tips, tricks and configuration info!:"
 echo "https://github.com/dtcooper/raspotify/wiki"
