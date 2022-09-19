1e8479dbba91e5c19477cb825ee938c63e5c6302
Automated Release <automated-release@users.noreply.github.com>
2022-09-19 03:50:32 +0000

    Update gh-pages apt repository (released tag 0.42.6)

--- asound_conf_setup.py
+++ asound_conf_setup.py
@@ -62,8 +62,7 @@ COMMON_RATES = [
 
 def privilege_check():
     try:
-        with open(DUMMY_PATH, "w") as _:
-            pass
+        open(DUMMY_PATH, "w").close()
         os.remove(DUMMY_PATH)
     except:
         print("Error: This script requires write privileges to /etc.")
@@ -103,9 +102,9 @@ def get_all_pcm_name():
     return all_pcm_names
 
 
-def get_hw_pcm_names(all_hw_pcm_names):
+def get_hw_pcm_names():    
     hw_pcm_names = [
-        n for n in all_hw_pcm_names.split("\n") if n.startswith("hw:")
+        n for n in get_all_pcm_name().split("\n") if n.startswith("hw:")
     ]
 
     if not hw_pcm_names:
@@ -130,14 +129,14 @@ def choose_hw_pcm(hw_pcm_names):
                 pcm = hw_pcm_names[int(choice) - 1]
             except KeyboardInterrupt:
                 revert_backup()
-                print("\n")
+                print("")
                 exit(0)
             except:
                 print("Invalid hw PCM: {}".format(choice))
                 print("Enter a number from 1 - {}.".format(len(hw_pcm_names)))
                 continue
             else:
-                return pcm
+                break
     else:
         pcm = hw_pcm_names[0]
 
@@ -146,7 +145,7 @@ def choose_hw_pcm(hw_pcm_names):
             "so that's what we'll use…".format(pcm)
         )
 
-        return pcm
+    return pcm
 
 
 def get_formats_and_rates(pcm):
@@ -211,14 +210,14 @@ def choose_format(formats):
                 format_ = formats[int(choice) - 1]
             except KeyboardInterrupt:
                 revert_backup()
-                print("\n")
+                print("")
                 exit(0)
             except:
                 print("Invalid format choice: {}".format(choice))
                 print("Enter a number from 1 - {}.".format(len(formats)))
                 continue
             else:
-                return format_
+                break
 
     else:
         format_ = formats[0]
@@ -228,7 +227,7 @@ def choose_format(formats):
             "so that's what we'll use…".format(format_)
         )
 
-        return format_
+    return format_
 
 
 def choose_rate(rates):
@@ -260,14 +259,14 @@ def choose_rate(rates):
                 rate = rates[int(choice) - 1]
             except KeyboardInterrupt:
                 revert_backup()
-                print("\n")
+                print("")
                 exit(0)
             except:
                 print("Invalid sampling rate choice: {}".format(choice))
                 print("Enter a number from 1 - {}.".format(len(rates)))
                 continue
             else:
-                return rate
+                break
 
     else:
         rate = rates[0]
@@ -284,7 +283,7 @@ def choose_rate(rates):
                 "glitches on low spec devices."
             )
 
-        return rate
+    return rate
 
 
 def pcm_to_card_device(pcm):
@@ -297,7 +296,7 @@ def pcm_to_card_device(pcm):
 
 
 def get_choices():
-    hw_pcm_names = get_hw_pcm_names(get_all_pcm_name())
+    hw_pcm_names = get_hw_pcm_names()
 
     while True:
         try:
@@ -320,7 +319,7 @@ def get_choices():
 
         except KeyboardInterrupt:
             revert_backup()
-            print("\n")
+            print("")
             exit(0)
 
 
@@ -341,7 +340,7 @@ def write_asound_conf():
             exit(0)
 
     except KeyboardInterrupt:
-        print("\n")
+        print("")
         exit(0)
 
     card, device, format_, rate = get_choices()
