008d5accad2510e3b82023d3f16cd38623456b69
Automated Release <automated-release@users.noreply.github.com>
2022-10-15 06:04:06 +0000

    Update gh-pages apt repository (released tag 0.42.6)

---
+++ awiz-install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+SUDO="sudo"
+APT="apt"
+
+if ! which sudo > /dev/null; then
+    SUDO=""
+
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "\nInsufficient privileges:\n"
+        echo "Please run this script as root."
+        exit 1
+    fi
+fi
+
+ARCH="$(dpkg-architecture -q DEB_TARGET_ARCH)"
+
+PACKAGE="asound-conf-wizard_0.1.1_$ARCH.deb"
+PACKAGE_URL="https://github.com/JasonLG1979/asound-conf-wizard/releases/download/0.1.1/$PACKAGE"
+
+wget -q "$PACKAGE_URL"
+
+$SUDO $APT update
+$SUDO $APT -y install "./$PACKAGE"
