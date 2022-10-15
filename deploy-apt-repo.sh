344d73e98d3cf8f6f47386adb92b10f1374a2c48
JasonLG1979 <jasonlevigray3@gmail.com>
2022-10-15 13:28:19 -0500

    Add asound-conf-wizard to the repo

--- deploy-apt-repo.sh
+++ deploy-apt-repo.sh
@@ -23,29 +23,53 @@ if [ -z "$RELEASE_TAG" ]; then
     exit 1
 fi
 
-if [ $(ls -1 *_armhf.deb | wc -l) -ne 1 ]; then
-    echo 'Exactly one *_armhf.deb package needs to be in folder.'
+if [ $(ls -1 raspotify*_armhf.deb | wc -l) -ne 1 ]; then
+    echo 'Exactly one raspotify*_armhf.deb package needs to be in folder.'
     exit 1
 fi
 
-if [ $(ls -1 *_arm64.deb | wc -l) -ne 1 ]; then
-    echo 'Exactly one *_arm64.deb package needs to be in folder.'
+if [ $(ls -1 raspotify*_arm64.deb | wc -l) -ne 1 ]; then
+    echo 'Exactly one raspotify*_arm64.deb package needs to be in folder.'
     exit 1
 fi
 
-if [ $(ls -1 *_amd64.deb | wc -l) -ne 1 ]; then
-    echo 'Exactly one *_amd64.deb package needs to be in folder.'
+if [ $(ls -1 raspotify*_amd64.deb | wc -l) -ne 1 ]; then
+    echo 'Exactly one raspotify*_amd64.deb package needs to be in folder.'
     exit 1
 fi
 
-ARMHF_DEB_PKG_NAME="$(ls -1 *_armhf.deb | head -n 1)"
-echo "Using package: $ARMHF_DEB_PKG_NAME"
+if [ $(ls -1 asound-conf-wizard*_armhf.deb | wc -l) -ne 1 ]; then
+    echo 'Exactly one asound-conf-wizard*_armhf.deb package needs to be in folder.'
+    exit 1
+fi
 
-ARM64_DEB_PKG_NAME="$(ls -1 *_arm64.deb | head -n 1)"
-echo "Using package: $ARM64_DEB_PKG_NAME"
+if [ $(ls -1 asound-conf-wizard*_arm64.deb | wc -l) -ne 1 ]; then
+    echo 'Exactly one asound-conf-wizard*_arm64.deb package needs to be in folder.'
+    exit 1
+fi
 
-AMD64_DEB_PKG_NAME="$(ls -1 *_amd64.deb | head -n 1)"
-echo "Using package: $AMD64_DEB_PKG_NAME"
+if [ $(ls -1 asound-conf-wizard*_amd64.deb | wc -l) -ne 1 ]; then
+    echo 'Exactly one asound-conf-wizard*_amd64.deb package needs to be in folder.'
+    exit 1
+fi
+
+RASPOTIFY_ARMHF_DEB_PKG_NAME="$(ls -1 raspotify*_armhf.deb | head -n 1)"
+echo "Using package: $RASPOTIFY_ARMHF_DEB_PKG_NAME"
+
+RASPOTIFY_ARM64_DEB_PKG_NAME="$(ls -1 raspotify*_arm64.deb | head -n 1)"
+echo "Using package: $RASPOTIFY_ARM64_DEB_PKG_NAME"
+
+RASPOTIFY_AMD64_DEB_PKG_NAME="$(ls -1 raspotify*_amd64.deb | head -n 1)"
+echo "Using package: $RASPOTIFY_AMD64_DEB_PKG_NAME"
+
+AWIZ_ARMHF_DEB_PKG_NAME="$(ls -1 asound-conf-wizard*_armhf.deb | head -n 1)"
+echo "Using package: $AWIZ_ARMHF_DEB_PKG_NAME"
+
+AWIZ_ARM64_DEB_PKG_NAME="$(ls -1 asound-conf-wizard*_arm64.deb | head -n 1)"
+echo "Using package: $AWIZ_ARM64_DEB_PKG_NAME"
+
+AWIZ_AMD64_DEB_PKG_NAME="$(ls -1 asound-conf-wizard*_amd64.deb | head -n 1)"
+echo "Using package: $AWIZ_AMD64_DEB_PKG_NAME"
 
 echo "Importing gpg key"
 echo "$GPG_SECRET_KEY_BASE64" | base64 -d | gpg --import
@@ -63,16 +87,24 @@ Architectures: armhf arm64 amd64
 SignWith: $GPG_KEY_ID
 EOF
 
-reprepro includedeb raspotify "../$ARMHF_DEB_PKG_NAME"
-reprepro includedeb raspotify "../$ARM64_DEB_PKG_NAME"
-reprepro includedeb raspotify "../$AMD64_DEB_PKG_NAME"
+reprepro includedeb raspotify "../$RASPOTIFY_ARMHF_DEB_PKG_NAME"
+reprepro includedeb raspotify "../$RASPOTIFY_ARM64_DEB_PKG_NAME"
+reprepro includedeb raspotify "../$RASPOTIFY_AMD64_DEB_PKG_NAME"
+reprepro includedeb raspotify "../$AWIZ_ARMHF_DEB_PKG_NAME"
+reprepro includedeb raspotify "../$AWIZ_ARM64_DEB_PKG_NAME"
+reprepro includedeb raspotify "../$AWIZ_AMD64_DEB_PKG_NAME"
 rm -rf conf db
 
-ln -fs "$(find . -name '*_armhf.deb' -type f -printf '%P\n' -quit)" raspotify-latest_armhf.deb
-ln -fs "$(find . -name '*_arm64.deb' -type f -printf '%P\n' -quit)" raspotify-latest_arm64.deb
-ln -fs "$(find . -name '*_amd64.deb' -type f -printf '%P\n' -quit)" raspotify-latest_amd64.deb
+ln -fs "$(find . -name 'raspotify*_armhf.deb' -type f -printf '%P\n' -quit)" raspotify-latest_armhf.deb
+ln -fs "$(find . -name 'raspotify*_arm64.deb' -type f -printf '%P\n' -quit)" raspotify-latest_arm64.deb
+ln -fs "$(find . -name 'raspotify*_amd64.deb' -type f -printf '%P\n' -quit)" raspotify-latest_amd64.deb
+ln -fs "$(find . -name 'asound-conf-wizard*_armhf.deb' -type f -printf '%P\n' -quit)" asound-conf-wizard-latest_armhf.deb
+ln -fs "$(find . -name 'asound-conf-wizard*_arm64.deb' -type f -printf '%P\n' -quit)" asound-conf-wizard-latest_arm64.deb
+ln -fs "$(find . -name 'asound-conf-wizard*_amd64.deb' -type f -printf '%P\n' -quit)" asound-conf-wizard-latest_amd64.deb
 
-echo "Repo created in directory apt-repo with packages $ARMHF_DEB_PKG_NAME, $ARM64_DEB_PKG_NAME and $AMD64_DEB_PKG_NAME"
+echo "Repo created in directory apt-repo with packages:"
+echo "$RASPOTIFY_ARMHF_DEB_PKG_NAME, $RASPOTIFY_ARM64_DEB_PKG_NAME, $RASPOTIFY_AMD64_DEB_PKG_NAME,"
+echo "$AWIZ_ARMHF_DEB_PKG_NAME, $AWIZ_ARM64_DEB_PKG_NAME, and $AWIZ_AMD64_DEB_PKG_NAME."
 
 gpg --armor --export "$GPG_KEY_ID" > key.asc
 cp -v ../README.md ../LICENSE ../install.sh ../awiz-install.sh .
