adeac72306612fb245ef078e56dece93b2a50254
JasonLG1979 <jasonlevigray3@gmail.com>
2022-11-26 00:42:33 -0600

    More system info

--- raspotify/usr/bin/raspotify-crash-report-generator.sh
+++ raspotify/usr/bin/raspotify-crash-report-generator.sh
@@ -2,16 +2,6 @@
 
 crash_report="/etc/raspotify/crash_report"
 
-echo -e "-- System Info --\n" > $crash_report
-
-uname -a >> $crash_report
-
-echo -e "\n-- Logs --\n" >> $crash_report
-
-journalctl -u raspotify --since "1min ago" -q >> $crash_report
-
-echo -e "\n-- Config --\n" >> $crash_report
-
 config="/etc/raspotify/conf"
 
 # We don't want user names or passwords in the crash report.
@@ -21,32 +11,40 @@ password="LIBRESPOT_PASSWORD"
 librespot="LIBRESPOT_"
 tmp_dir="TMPDIR"
 
+logs=$(journalctl -u raspotify --since "1min ago" -q)
+
+fail_count=$(echo "$logs" | grep -o "raspotify.service: Failed" | wc -l)
+
+{
+  echo -e "-- System Info --\n"
+  uname -a
+  cat /etc/os-release
+  echo -e "\n-- Logs --\n"
+  echo "$logs"
+  echo -e "\n-- Config --\n"
+} > $crash_report 2> /dev/null
+
 while read -r line; do
 if { [[ $line = $librespot* ]] && [[ $line != $username* ]] && [[ $line != $password* ]]; } || [[ $line = $tmp_dir* ]]
 then
-    echo "$line" >> $crash_report
+  echo "$line" >> $crash_report 2> /dev/null
 fi
 done < $config
 
-echo -e "\n-- Ouput of aplay -l --\n" >> $crash_report
-
-aplay -l >> $crash_report
-
-echo -e "\n-- Ouput of aplay -L --\n" >> $crash_report
-
-aplay -L >> $crash_report
-
-echo -e "\n-- Ouput of librespot -d ? --" >> $crash_report
-
-librespot -d ? >> $crash_report 2> /dev/null
+{
+  echo -e "\n-- Ouput of aplay -l --\n"
+  aplay -l
+  echo -e "\n-- Ouput of aplay -L --\n"
+  aplay -L
+  echo -e "\n-- Ouput of librespot -d ? --"
+  librespot -d "?"
+} >> $crash_report 2> /dev/null
 
 systemctl reset-failed raspotify
 
-fail_count=$(journalctl -u raspotify --since "1min ago" -q | grep -o "raspotify.service: Failed" | wc -l)
-
 if [ "$fail_count" -lt 6 ]
 then
-    # delay restarts by 10 sec to debounce
-    sleep 10
-    systemctl restart raspotify
+  # delay restarts by 10 sec to debounce
+  sleep 10
+  systemctl restart raspotify
 fi
