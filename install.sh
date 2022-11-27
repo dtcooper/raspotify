77f325d2315b148357ce5bd1007eb99cd77760ec
Automated Release <automated-release@users.noreply.github.com>
2022-11-27 06:45:08 +0000

    Update gh-pages apt repository (released tag 0.43.10)

--- install.sh
+++ install.sh
@@ -20,79 +20,85 @@ REQ_PACKAGES="libc6 systemd init-system-helpers libasound2 alsa-utils libpulse0"
 PACKAGES_TO_INSTALL=
 MIN_NOT_MET=
 
-if ! which apt > /dev/null; then
-    APT="apt-get"
+if ! which apt >/dev/null; then
+	APT="apt-get"
 
-    if ! which apt-get > /dev/null; then
-        echo "\nUnspported OS:\n"
-        echo "$ERROR_MESG"
-        exit 1
-    fi
+	if ! which apt-get >/dev/null; then
+		echo "\nUnspported OS:\n"
+		echo "$ERROR_MESG"
+		exit 1
+	fi
 fi
 
 if uname -a | grep -F -ivq -e armv7 -e aarch64 -e x86_64; then
-    echo "\nUnspported architecture:\n"
-    echo "$ERROR_MESG"
-    echo "\nSupport for ARMv6 (Pi v1 and Pi Zero v1.x) has been dropped."
-    echo "0.31.8.1 was the last version to be built with ARMv6 support."
-    echo "\nhttps://github.com/dtcooper/raspotify/releases/tag/0.31.8.1\n"
-    echo "You can install and run that version on an ARMv6 device,"
-    echo "but you will never get updates and doing so is completely unsupported."
-    exit 1
+	echo "\nUnspported architecture:\n"
+	echo "$ERROR_MESG"
+	echo "\nSupport for ARMv6 (Pi v1 and Pi Zero v1.x) has been dropped."
+	echo "0.31.8.1 was the last version to be built with ARMv6 support."
+	echo "\nhttps://github.com/dtcooper/raspotify/releases/tag/0.31.8.1\n"
+	echo "You can install and run that version on an ARMv6 device,"
+	echo "but you will never get updates and doing so is completely unsupported."
+	exit 1
 fi
 
-if ! which sudo > /dev/null; then
-    SUDO=""
+if ! which sudo >/dev/null; then
+	SUDO=""
 
-    if [ "$(id -u)" -ne 0 ]; then
-        echo "\nInsufficient privileges:\n"
-        echo "Please run this script as root."
-        exit 1
-    fi
+	if [ "$(id -u)" -ne 0 ]; then
+		echo "\nInsufficient privileges:\n"
+		echo "Please run this script as root."
+		exit 1
+	fi
 fi
 
 for package in $REQ_PACKAGES; do
-    if ! dpkg-query -W -f='${db:Status-Status}\n' "$package" 2> /dev/null | grep -q '^installed$'; then
-        PACKAGES_TO_INSTALL="$PACKAGES_TO_INSTALL $package"
-    fi
+	if ! dpkg-query -W -f='${db:Status-Status}\n' "$package" 2>/dev/null | grep -q '^installed$'; then
+		PACKAGES_TO_INSTALL="$PACKAGES_TO_INSTALL $package"
+	fi
 done
 
 if [ "$PACKAGES_TO_INSTALL" ]; then
-    $SUDO $APT update
-    $SUDO $APT -y install $PACKAGES_TO_INSTALL
+	$SUDO $APT update
+	$SUDO $APT -y install $PACKAGES_TO_INSTALL
 fi
 
 for package in $REQ_PACKAGES; do
-    case "$package" in
-       "libc6") MIN_VER=$LIBC_MIN_VER
-       ;;
-       "systemd") MIN_VER=$SYSTEMD_MIN_VER
-       ;;
-       "libasound2") MIN_VER=$LIBASOUND_MIN_VER
-       ;;
-       "alsa-utils") MIN_VER=$ALSA_UTILS_VER
-       ;;
-       "libpulse0") MIN_VER=$LIBPULSE_MIN_VER
-       ;;
-       "init-system-helpers") MIN_VER=$HELPER_MIN_VER
-       ;;
-    esac
+	case "$package" in
+	"libc6")
+		MIN_VER=$LIBC_MIN_VER
+		;;
+	"systemd")
+		MIN_VER=$SYSTEMD_MIN_VER
+		;;
+	"libasound2")
+		MIN_VER=$LIBASOUND_MIN_VER
+		;;
+	"alsa-utils")
+		MIN_VER=$ALSA_UTILS_VER
+		;;
+	"libpulse0")
+		MIN_VER=$LIBPULSE_MIN_VER
+		;;
+	"init-system-helpers")
+		MIN_VER=$HELPER_MIN_VER
+		;;
+	esac
 
-    VER="$(dpkg-query -W -f='${Version}' $package)"
+	VER="$(dpkg-query -W -f='${Version}' $package)"
 
-    if eval dpkg --compare-versions "$VER" lt "$MIN_VER"; then
-        MIN_NOT_MET="$MIN_NOT_MET$package >= $MIN_VER is required but $VER is installed.\n"
-    fi
+	if eval dpkg --compare-versions "$VER" lt "$MIN_VER"; then
+		MIN_NOT_MET="$MIN_NOT_MET$package >= $MIN_VER is required but $VER is installed.\n"
+	fi
 done
 
 if [ "$MIN_NOT_MET" ]; then
-    echo "\nUnmet minimum required package version(s):\n"
-    echo "$MIN_NOT_MET"
-    echo "$ERROR_MESG"
-    exit 1
+	echo "\nUnmet minimum required package version(s):\n"
+	echo "$MIN_NOT_MET"
+	echo "$ERROR_MESG"
+	exit 1
 fi
 
-curl -sSL https://dtcooper.github.io/raspotify/key.asc | $SUDO tee /usr/share/keyrings/raspotify_key.asc > /dev/null
+curl -sSL https://dtcooper.github.io/raspotify/key.asc | $SUDO tee /usr/share/keyrings/raspotify_key.asc >/dev/null
 $SUDO chmod 644 /usr/share/keyrings/raspotify_key.asc
 echo "$SOURCE_REPO" | $SUDO tee /etc/apt/sources.list.d/raspotify.list
 
