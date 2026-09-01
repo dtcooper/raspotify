dtcooper/raspotify

commit afb86cf085f842e14f55d2c1eb72871fdd778435

Only copy over copyright files once

build.sh
@@ -14,7 +14,6 @@ cd /mnt/raspotify
 RASPOTIFY_GIT_VER="$(git describe --tags "$(git rev-list --tags --max-count=1)" 2>/dev/null || echo unknown)"
 
 if [ ! -d librespot ]; then
-	echo "No directory named librespot exists! Cloning..."
 	# Use a vendored version of librespot.
 	# https://github.com/librespot-org/librespot does not regularly or really ever update their dependencies on released versions.
 	# https://github.com/librespot-org/librespot/pull/1068
@@ -22,6 +21,12 @@ if [ ! -d librespot ]; then
 	cd librespot
 	git checkout raspotify
 	cd /mnt/raspotify
+	# Copy over copyright files
+	DOC_DIR="raspotify/usr/share/doc/raspotify"
+	mkdir -p "$DOC_DIR"
+	cp -v LICENSE "$DOC_DIR/copyright"
+	cp -v readme "$DOC_DIR/readme"
+	cp -v librespot/LICENSE "$DOC_DIR/librespot.copyright"
 fi
 
 cd librespot
@@ -38,13 +43,6 @@ cd /mnt/raspotify
 
 cp -v /build/"$BUILD_TARGET"/raspotify/librespot raspotify/usr/bin
 
-# Copy over copyright files
-DOC_DIR="raspotify/usr/share/doc/raspotify"
-mkdir -p "$DOC_DIR"
-cp -v LICENSE "$DOC_DIR/copyright"
-cp -v readme "$DOC_DIR/readme"
-cp -v librespot/LICENSE "$DOC_DIR/librespot.copyright"
-
 # Compute final package version + filename for Debian control file
 DEB_PKG_VER="${RASPOTIFY_GIT_VER}~librespot.${LIBRESPOT_VER}-${LIBRESPOT_HASH}"
 DEB_PKG_NAME="raspotify_${DEB_PKG_VER}_${ARCHITECTURE}.deb"
@@ -55,7 +53,6 @@ DEB_PKG_NAME="raspotify_${DEB_PKG_VER}_${ARCHITECTURE}.deb"
 INSTALLED_SIZE="$((($(du -bs raspotify --exclude=raspotify/DEBIAN/control | cut -f 1) + 2048) / 1024))"
 
 export DEB_PKG_VER
-export RUST_VER=$(rustc -V)
 export INSTALLED_SIZE
 envsubst <control.debian.tmpl >raspotify/DEBIAN/control
 
@@ -69,7 +66,6 @@ echo "Raspotify package estimated Size $PACKAGE_SIZE (Bytes)"
 echo "Raspotify package estimated Installed Size $INSTALLED_SIZE (KiB)"
 
 if [ ! -d asound-conf-wizard ]; then
-	echo "No directory named asound-conf-wizard exists! Cloning..."
 	git clone https://github.com/JasonLG1979/asound-conf-wizard.git
 fi
 

control.debian.tmpl
@@ -11,4 +11,4 @@ Vcs-Browser: https://github.com/dtcooper/raspotify
 Vcs-Git: https://github.com/dtcooper/raspotify
 Description: Raspotify
  A Spotify Connect systemd daemon for Debian Stable based headless systems which thinly wraps a fork of librespot.
- [Built with ${RUST_VER}]
+ [Built with rustc ${RUST_VERSION}]

commit 760ec20c141013647075b8ddf02c1bea22b2ef06

build: export RUST_VERSION so package description isn't blank
RUST_VERSION was never exported before the envsubst call that
generates DEBIAN/control, so every built package's description ends
with the literal, unfilled "[Built with rustc ]". This regressed in
afb86cf when the export was dropped while switching away from
jinja2 templating.

build.sh
@@ -74,8 +74,10 @@ packages() {
 	INSTALLED_SIZE="$((($(du -bs raspotify --exclude=raspotify/DEBIAN/control | cut -f 1) + 2048) / 1024))"
 
 	echo "Generate Debian control..."
+	RUST_VERSION="$(rustc -V | cut -d' ' -f2-)"
 	export DEB_PKG_VER
 	export INSTALLED_SIZE
+	export RUST_VERSION
 	envsubst <control.debian.tmpl >raspotify/DEBIAN/control
 
 	echo "Build Raspotify deb..."